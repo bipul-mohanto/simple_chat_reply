cc3d80368581d4abbc1620084af4db2f79359a37
Bipul Mohanto <bipul.mohanto@yahoo.com>
2020-07-10 10:06:32 +0600

    test for git-upload

---
+++ FirstWebSiteCPP/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Containt-type:text/html \n\n";
+	cout << "<html>";
+	cout << "<center>";
+	cout << "<head><title> Hello </title></head>";
+	cout << "<body>";
+	cout << "<h1> Hello World! </h1>";
+	cout << "<h1>End of Something but not working still </h1>";
+	cout << "<h3> This is actually fun, but what is the benefit using C++ as HTML? </h3>";
+	cout << "</center>";
+	cout << "</body> </html>";
+	cin.get(); 
+	return 0;
+}
